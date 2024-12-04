6eea30c2b494db45d5c77661d0d5da65b5105003
Darylld Tupaz <161782745+Darylldd@users.noreply.github.com>
2024-12-04 20:55:45 +0800

    Update forms.py

--- text_summarizer/summarizer/forms.py
+++ text_summarizer/summarizer/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class FileUploadForm(forms.Form):
     file = forms.FileField()
-    prompt = forms.CharField(widget=forms.Textarea, required=False, label="Prompt")
+    prompt = forms.CharField(max_length=255, required=False, widget=forms.TextInput(attrs={'placeholder': 'Enter prompt'}))
